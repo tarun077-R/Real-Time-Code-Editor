4e00761a8ba3e28a81b09b702c3842b1209b5fa6
Tarun Rawat <tarunrawat8350@gmail.com>
2026-06-29 12:54:46 +0530

    Setup collaborative editor

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import {Editor} from "@monaco-editor/react"
+import { MonacoBinding} from "y-monaco"
+import {useRef,useMemo} from "react"
+import * as Y from "yjs";
+import { SocketIOProvider } from 'y-socket.io';
+
+
 const App = () => {
+  const editorRef = useRef(null)
+  const ydoc = useMemo(()=>new Y.Doc(),[])
+const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+  const handleMount = (editor)=>{
+editorRef.current = editor
+const provider = new SocketIOProvider("http://localhost:3000","monaco",ydoc,{
+  autoConnect:true
+})
+const monacoBinding = new MonacoBinding(
+  yText,
+  editorRef.current.getModel(),
+  new Set([editorRef.current]),
+  provider.awareness
+)
+  }
   return (
     <main className='h-screen w-full bg-gray-950 flex gap-4 p-2'>
       <aside className='h-full w-1/4 bg-amber-50  rounded-lg'>
@@ -12,6 +33,7 @@ height="100%"
 defaultLanguage="javascript"
 defaultValue="// some comment"
 theme="vs-dark"
+onMount={handleMount}
 />
       </section>
 
